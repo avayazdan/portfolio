8de1d2f34e0fff62a509c1ec435b5b003cb638c8
Ava Yazdany <avayaz1337@gmail.com>
2022-06-11 01:57:52 +0200

    set up complete and initial html added

---
+++ src/components/Home.js
@@ -0,0 +1,109 @@
+import React from "react"
+// import "../home.css"
+import { Link } from "react-router-dom";
+
+
+
+{/* <link rel="stylesheet" href="home.css"></link> */}
+
+function Home() {
+  return (
+    <><div class="h1-box">
+      <div class="typewrite">
+        <h1>Hi, I'm Ava...</h1>
+      </div>
+    </div><h2>Full Stack Software Engineer</h2><div class="nav-container">
+        <nav class="navigation">
+          <ul>
+
+            <a href='#'>
+              <li>About me &nbsp; &nbsp;</li>
+            </a>
+
+
+            <a href='#'>
+              <li>Mission & Work Ethic &nbsp; &nbsp;</li>
+            </a>
+
+            <a href='#'>
+              <li>Skills &nbsp; &nbsp;</li>
+            </a>
+
+
+            <a href='#'>
+              <li>Projects&nbsp; &nbsp;</li>
+            </a>
+
+
+            <a href='#'>
+              <li>Contact</li>
+            </a>
+
+
+          </ul>
+        </nav>
+      </div><div class="ball-body">
+        <div class="ball">
+        </div>
+      </div><section id="about">
+        <div class="p">
+          <p>Hi, I'm Ava. I'm a Full Stack Software Engineer with a passion for
+            technology, learning, and creating.<br></br>
+              Currently based in London, United Kingdom.</p>
+          <p>Creative arts - I have dedicated most of my life to the creative arts, specifically in music, where I
+            achieved the most out of all of my creative endeavours. Some highlights of my music career were playing
+            national shows, playing sets and featuring on various online Radio such as NTS and Radar Radio,
+            collaborating with artists from all over the world, directing my own music videos, and earning 265K
+            plays in total on SoundCloud. I am self-taught in screenwriting and have written two feature-length film
+            scripts. Last year I released a short poetry anthology and I currently write on Substack. I am also in
+            the process of creating my first few video essays, which aim to analyse media. I am extremely passionate
+            about writing, collaborating with others sonically and cult films and have succeeded in all of these
+            areas in my own way.
+            The humanities - I have a love for history, specifically history of art, which is why I have a small
+            Instagram page dedicated to my own personal curation of my favourite moments in art history. I am also
+            passionate about Western Philosophy, language learning, psychology and logical reasoning in social
+            politics.
+            Interests and more about me:
+            I have dedicated most of my life to the creative arts, specifically in music, where I achieved the most out of all of my creative endeavours. Some highlights of my music career were playing national shows, playing sets and featuring on various online Radio such as NTS and Radar Radio, collaborating with artists from all over the world, directing my own music videos, and earning over 265K plays in total on SoundCloud. I am self-taught in screenwriting and have written two feature-length film scripts. Last year, I self-published a short poetry anthology and I currently write on Substack. I am extremely passionate about writing, collaborating with others sonically, cult cinema, and have succeeded in all of these areas in my own way. I also have a love for languages and the humanities, and have taught myself a lot about these subjects in recent years. I hope to carry on growing my knowledge in all areas I am interested in, both technical and artistic in the years to come.</p>
+        </div>
+      </section><div class="button">
+        <button>Hello</button>
+
+      </div><h3 align="left">:wrench: Languages and Tools:</h3><br></br><p align="left"><a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img
+        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+        alt="css3" width="40" height="40" /> </a> <a href="https://www.djangoproject.com/" target="_blank"
+          rel="noreferrer"> <img src https />://preneure.com/wp-content/uploads/2022/02/django.png" alt="django"
+          width="40" height="40" /> </a> <a href="https://expressjs.com" target="_blank" rel="noreferrer">
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+            alt="express" width="40" height="40" /> </a> <a href="https://git-scm.com/" target="_blank"
+              rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git"
+                width="40" height="40" /> </a> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+            alt="html5" width="40" height="40" /> </a> <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+            alt="javascript" width="40" height="40" /> </a> <a href="https://jestjs.io" target="_blank"
+              rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg" alt="jest"
+                width="40" height="40" /> </a> <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+          <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+            alt="mongodb" width="40" height="40" /> </a> <a href="https://www.mysql.com/" target="_blank"
+              rel="noreferrer"> <img
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
+            alt="mysql" width="40" height="40" /> </a> <a href="https://nodejs.org" target="_blank"
+              rel="noreferrer"> <img
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+            alt="nodejs" width="40" height="40" /> </a> <a href="https://www.postgresql.org" target="_blank"
+              rel="noreferrer"> <img
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg"
+            alt="postgresql" width="40" height="40" /> </a> <a href="https://www.python.org" target="_blank"
+              rel="noreferrer"> <img
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
+            alt="python" width="40" height="40" /> </a> <a href="https://reactjs.org/" target="_blank"
+              rel="noreferrer"> <img
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+            alt="react" width="40" height="40" /> </a> </p><br></br>
+              </>
+  )
+}
+
+export default Home
---
+++ src/components/Nav.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+// import "../Nav.css"
+
+{/* <link rel="stylesheet" href="Nav.css"></link> */}
+
+
+function Nav() {
+    return (
+        <>
+            <header>
+                <nav className="nav-bar">
+                  {/* <img src={WalkiesLogo} alt="Walkies logo"></img> */}
+                    <ul>
+                        <li>
+                            <Link to="/"> Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/dogs"> View Dogs</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Register</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/messages">Messages</Link>
+                        </li>
+                        <li>
+                            <Link to="/dogs/mydog">My Dogs</Link>
+                        </li>
+
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+            <div className="footer-container">
+              <div className="social-container">
+              {/* <a href ="https://www.facebook.com" target="_blank" rel="noreferrer noopener"><img src ={FacebookLogo} alt ="facebook logo"></img></a>
+              <a href ="https://www.twitter.com" target="_blank" rel="noreferrer noopener"><img src ={TwitterLogo} alt ="twitter logo"></img></a>
+              <a href ="https://www.instagram.com" target="_blank" rel="noreferrer noopener"><img src ={InstagramLogo} alt ="instagram logo"></img></a> */}
+              </div>
+            <footer>
+              <p className="footer">&copy;Walkies 2022</p>
+              <div className="contact-container">
+              <p className="footer">Contact us:<br>
+              </br><br></br>Phone: 0207 637 289<br>
+              </br>Email: info@walkies.com<br>
+              </br>Address: 129<br></br>
+              Holloway Road<br></br>
+              NW10 7H2</p>
+              </div>
+
+            </footer>
+            </div>
+        </>
+    );
+}
+export default Nav;
