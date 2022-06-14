213582ff6e8c21ce7ec2f744273fc7d77618ed3b
Ava Yazdany <avayaz1337@gmail.com>
2022-06-14 02:22:34 +0200

    content and styling update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,12 @@
 import React from "react"
 // import "../home.css"
 import { Link } from "react-router-dom";
+import HomepageOne from "../assets/3d1.jpg"
+import HomepageTwo from "../assets/3d3.jpg"
+import HomepageThree from "../assets/3d5.png"
+
+
+
 
 
 
@@ -27,6 +33,7 @@ function Home() {
       </div>
       <div className="container-2">
     <div className="nav-container">
+      
         <nav className="navigation">
           <ul>
 
@@ -57,16 +64,24 @@ function Home() {
           </ul>
         </nav>
         </div>
+        <div className="images-container">
+      <img className="homepage" src = {HomepageTwo} alt = "Walkies logo"/>
+      <img className="homepage" src = {HomepageOne} alt = "Walkies logo"/>
+      <img className="homepage" src = {HomepageThree} alt = "Walkies logo"/>
+
+
+        </div>
       </div>
       <div class="ball-body">
         <div class="ball">
         </div>
-      </div><section id="about">
-        <div class="p">
+      </div>
+      <section id="about">
+        <div className="container-3">
+              <h3>A little about me...</h3>
           <p>Hi, I'm Ava. I'm a Full Stack Software Engineer with a passion for
             technology, learning, and creating.<br></br>
               Currently based in London, United Kingdom.</p>
-              <h3>More about me</h3>
           <p>Creative arts - I have dedicated most of my life to the creative arts, specifically in music, where I
             achieved the most out of all of my creative endeavours. Some highlights of my music career were playing
             national shows, playing sets and featuring on various online Radio such as NTS and Radar Radio,
@@ -83,9 +98,9 @@ function Home() {
             Interests and more about me:
             I have dedicated most of my life to the creative arts, specifically in music, where I achieved the most out of all of my creative endeavours. Some highlights of my music career were playing national shows, playing sets and featuring on various online Radio such as NTS and Radar Radio, collaborating with artists from all over the world, directing my own music videos, and earning over 265K plays in total on SoundCloud. I am self-taught in screenwriting and have written two feature-length film scripts. Last year, I self-published a short poetry anthology and I currently write on Substack. I am extremely passionate about writing, collaborating with others sonically, cult cinema, and have succeeded in all of these areas in my own way. I also have a love for languages and the humanities, and have taught myself a lot about these subjects in recent years. I hope to carry on growing my knowledge in all areas I am interested in, both technical and artistic in the years to come.</p>
         </div>
-      </section><div class="button">
-        <button>Hello</button>
-
+      </section>
+      <div class="button">
+        <button></button>
       </div><h3 align="left">:wrench: Languages and Tools:</h3><br></br><p align="left"><a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img
         src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
         alt="css3" width="40" height="40" /> </a> <a href="https://www.djangoproject.com/" target="_blank"
